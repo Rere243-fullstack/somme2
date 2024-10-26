110b203f2d062ffc069c148655ec3eae99cb87a9
Rere243-fullstack <rethambombo@gmail.com>
2024-10-26 04:10:30 +0200

    craetion d'une structure html et js

---
+++ test/index.js
@@ -0,0 +1,3 @@
+let baliseImage = document.getElementById("premiereImage");
+baliseImage.setAttribute("alt", "Ceci est la nouvelle valeur de alt");
+baliseImage.alt = "Ceci est une image de test modifiée";
